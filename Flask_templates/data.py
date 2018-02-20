5dba874cc3a05b65aaa9f839fb050ec0535cecae
Nomwesigwa Brian <bnomwesigwa@gmail.com>
2018-02-20 14:09:00 +0300

    Adding part of the flask application

---
+++ Flask_templates/data.py
@@ -0,0 +1,25 @@
+def Business():
+    business = [
+        {
+            'id':1,
+            'name':'1st Business',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt tempore esse quod nisi inventore minus sit id officia natus optio molestiae architecto voluptatem, similique distinctio.Illo iste voluptas placeat doloribus.',
+            'author':'Brian Van',
+            'create_date':'09-02-2018'
+        },
+        {
+            'id': 2,
+            'name': '2nd Business',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt tempore esse quod nisi inventore minus sit id officia natus optio molestiae architecto voluptatem, similique distinctio.Illo iste voluptas placeat doloribus.',
+            'author': 'Brian Nomwesigwa',
+            'create_date': '09-02-2018'
+        },
+        {
+            'id': 3,
+            'name': '3rd Business',
+            'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt tempore esse quod nisi inventore minus sit id officia natus optio molestiae architecto voluptatem, similique distinctio.Illo iste voluptas placeat doloribus.',
+            'author': 'Brian Don',
+            'create_date': '09-02-2018'
+        }
+    ]
+    return business
